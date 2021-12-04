86107487d0a54fca21e50e8dd8383a8e2d87e3db
Eddy Jansson <eddy@klopper.net>
2021-12-04 21:08:21 +0100

    Basic radix_sort_CE0 with merged c+b arrays.
    The reference pseudo-code uses two separate
    |Σ|-sized arrays to track counts and bucket
    offsets.
    
    I don't see why we shouldn't merge them into
    one, and calculate the counts by subtracting
    two adjacent bucket pointers.
    
    If it turns out this doesn't work, I guess that's
    egg on my face.

--- sort_ce0.c
+++ sort_ce0.c
@@ -4,8 +4,6 @@
 #include <string.h>
 #include <assert.h>
 
-#define SWAP(a, b) do { __auto_type T = (a); a = b; b = T; } while (0)
-
 #ifndef RESTRICT
 #define RESTRICT __restrict__
 #endif
@@ -23,55 +21,40 @@ struct sortrec const source_arr[] = {
 	{ 2, "abracadabrakusinvitamin" },
 	{ 45, "klopper" },
 	{ 255, "" },
+	{ 13, "klöpper" },
 	{ 1, "klopper!aksjdkasjdk" },
 };
 
 static const char** radix_sort_CE0(const char** RESTRICT S, const char** RESTRICT T, size_t n, int h) {
-	size_t c[256] = { 0 };
-	size_t b[256] = { 0 };
-	size_t y[256] = { 0 };
-
-	// printf("Sorting %zu entries, prefix len %d -- S @ %p, T @ %p\n", n, h, S, T);
-	// TODO: return if n < 2?
+	size_t cb[256] = { 0 };
 
 	// Generate histogram/character counts
 	for (size_t i = 0 ; i < n ; ++i) {
-		// printf(">'%s' <- %d from %s'\n", S[i] + h, h, S[i]);
-		++c[(uint8_t)(S[i][h])];
-		++y[(uint8_t)(S[i][h])];
+		++cb[(uint8_t)(S[i][h])];
 	}
 
-	// Generate prefix sums
-	b[0] = 0;
-	for (size_t i = 1 ; i < 256 ; ++i) {
-		b[i] = b[i-1] + c[i-1];
-	}
 	size_t x = 0;
 	for (size_t i = 0 ; i < 256 ; ++i) {
-		size_t a = y[i];
-		y[i] = x;
+		size_t a = cb[i];
+		cb[i] = x;
 		x += a;
-		printf("b[%zu]=%zu, y:=%zu\n", i, b[i], y[i]);
-		assert(b[i] == y[i]);
 	}
 
 	// Sort
 	for (size_t i = 0 ; i < n ; ++i) {
 		uint8_t idx = S[i][h];
-		T[b[idx]] = S[i];
-		++b[idx];
+		T[cb[idx]] = S[i];
+		++cb[idx];
 	}
-	memcpy(S, T, n * sizeof(const char*));
-
-	assert(c[0] == b[1]);
+	memcpy(S, T, n * sizeof(*S));
 
 	// Recursively sort buckets with more than one suffixes left.
-	x = b[1]; // These are the zero-terminators, which we skip.
+	x = cb[1]; // These are the zero-terminators, which we skip.
 	for (size_t i = 1 ; i < 256 ; ++i) {
-		size_t ci = b[i] - b[i-1];
-		assert(c[i] == ci);
+		size_t ci = cb[i] - cb[i-1];
 		if (ci > 1) {
-			printf("recurse S+%zu, n=%zu, h=%d\n", x, ci, h+1);
+			// printf("recurse S+%zu, n=%zu, h=%d\n", x, ci, h+1);
+			// We could call different sorts here, e.g for small n.
 			radix_sort_CE0(S+x, T, ci, h+1);
 		}
 		x += ci;
