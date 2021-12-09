1a5096350aacd21f2253a9f37c7e0d43a8fa1c06
Eddy Jansson <eddy@klopper.net>
2021-12-09 16:28:48 +0100

    Add algorithm selection to driver.
    Turn existing statistics collection into a bunch of macros,
    so they're easy to turn off.

--- sorter.c
+++ sorter.c
@@ -13,40 +13,18 @@
 #define RESTRICT __restrict__
 #endif
 
-#if 0
-static const char** radix_sort_CE0(const char** RESTRICT S, const char** RESTRICT T, size_t n, int h) {
-	size_t c[256] = { 0 };
-	size_t b[256];
+typedef const char** (*radix_sorter_fp)(const char** RESTRICT, const char** RESTRICT, size_t, int);
 
-	// Generate histogram
-	for (size_t i = 0 ; i < n ; ++i) {
-		++c[(uint8_t)(S[i][h])];
-	}
-
-	// Generate prefix sums
-	b[0] = 0;
-	for (size_t i = 1 ; i < 256 ; ++i) {
-		b[i] = b[i-1] + c[i-1];
-	}
-
-	// Sort
-	for (size_t i = 0 ; i < n ; ++i) {
-		uint8_t idx = S[i][h];
-		T[b[idx]++] = S[i];
-	}
-	memcpy(S, T, n * sizeof(*S));
-
-	// Recursively sort buckets, skipping the first which contains strings already in order.
-	size_t x = c[0];
-	for (size_t i = 1 ; i < 256 ; ++i) {
-		if (c[i] > 1) {
-			radix_sort_CE0(S+x, T+x, c[i], h+1);
-		}
-		x += c[i];
-	}
-
-	return S;
-}
+#if defined STATS
+#define STAT_INC_CALLS ++calls
+#define STAT_INC_ITERS ++iters
+#define STAT_INC_WASTED_ITERS ++wasted_iters
+#define STAT_INC_BUCKET(bucket) ++bucket_use[(bucket)]
+#else
+#define STAT_INC_CALLS
+#define STAT_INC_ITERS
+#define STAT_INC_WASTED_ITERS
+#define STAT_INC_BUCKET(bucket)
 #endif
 
 static size_t calls = 0;
@@ -54,82 +32,29 @@ static size_t iters = 0;
 static size_t wasted_iters = 0;
 static size_t bucket_use[256] = { 0 };
 
-static const char** radix_sort_CE0_CB_BM1(const char** RESTRICT S, const char** RESTRICT T, size_t n, int h) {
-	uint64_t buse[4] = { 0 };
-	uint64_t cuse[4] = { 0 }; // TODO: Could overwrite buse during prefix sum iteration (after k loop)
-	uint8_t bones[256] = { 0 };
-	size_t cb[256] = { 0 };
+#include "radix_sort_CE0.c"
+#include "radix_sort_CE0_CB.c"
+#include "radix_sort_CE0_CB_BM1.c"
 
-	++calls;
-
-	// Generate histogram/character counts
-	for (size_t i = 0 ; i < n ; ++i) {
-		uint8_t idx = S[i][h];
-		++cb[idx];
-		buse[idx >> 6] |= (1UL << (idx & 63));
-	}
-
-	size_t x = 0;
-	uint8_t ones_skipped = 0;
-	for (int k = 0 ; k < 4 ; ++k) {
-		uint64_t bitset = buse[k];
-		while (bitset != 0) {
-			size_t i = __builtin_ctzl(bitset) + (k * 64);
-
-			size_t a = cb[i];
-			cb[i] = x;
-			x += a;
-
-			if (a > 1) {
-				cuse[i >> 6] |= (1UL << (i & 63));
-				bones[i] = ones_skipped;
-				ones_skipped = 0;
-			} else if (a == 1 && i > 0) {
-				++ones_skipped;
-			}
-
-			bitset ^= (bitset & -bitset);
-		}
-	}
-
-	// Sort
-	for (size_t i = 0 ; i < n ; ++i) {
-		uint8_t idx = S[i][h];
-		// assert((buse[idx >> 6] & (1UL << (idx & 63))) != 0);
-		T[cb[idx]++] = S[i];
-	}
-	memcpy(S, T, n * sizeof(*S));
-
-	// Recursively sort buckets with more than one suffixes left.
-	x = cb[0]; // This should be the count of '\0' in input.
-	cuse[0] &= ~1UL;
-	for (int k = 0 ; k < 4 ; ++k) {
-		uint64_t bitset = cuse[k];
-		while (bitset != 0) {
-			size_t i = __builtin_ctzl(bitset) + (k * 64);
-
-			assert(i > 0);
-			assert(cb[i] >= x);
-			++bucket_use[i];
-			++iters;
-
-			// The count needs to be adjusted by 1 for every one-suffix iteration we've skipped over.
-			x += bones[i];
-			size_t ci = (cb[i] - x);
-
-			assert(ci > 1);
-			radix_sort_CE0_CB_BM1(S+x, T, ci, h+1);
-			x = cb[i];
-
-			bitset ^= bitset & -bitset;
-		}
-	}
-
-	return S;
-}
+struct radix_sorter_t {
+	const char *name;
+	radix_sorter_fp func;
+} radix_sorters[] = {
+	{
+		.name = "MSD Radix Sort variant CE0",
+		.func = radix_sort_CE0
+	},
+	{
+		.name = "MSD Radix Sort variant CE0_CB",
+		.func = radix_sort_CE0_CB
+	},
+	{
+		.name = "MSD Radix Sort variant CE0_CB_BM1",
+		.func = radix_sort_CE0_CB_BM1
+	},
+};
 
 static int map_file(const char *filename, void **data, size_t *size) {
-
 	printf("mmap'ing '%s'\n", filename);
 	FILE *f = fopen(filename, "rb");
 	if (!f) {
@@ -156,7 +81,7 @@ static int map_file(const char *filename, void **data, size_t *size) {
 	return 0;
 }
 
-static size_t generate_string_array(char *data, size_t len, const char ***arr) {
+static size_t generate_string_ptrs(char *data, size_t len, const char ***arr) {
 	size_t entries = 0;
 
 	char *p = data;
@@ -185,9 +110,20 @@ static size_t generate_string_array(char *data, size_t len, const char ***arr) {
 	return entries;
 }
 
-
 int main(int argc, char *argv[]) {
+	const size_t NUM_VARIANTS = sizeof(radix_sorters)/sizeof(radix_sorters[0]);
+
 	const char *filename = argc > 1 ? argv[1] : "../test-shuffled.txt";
+	int variant = argc > 2 ? atoi(argv[2]) : (int)(NUM_VARIANTS-1);
+
+	if (variant >= (int)NUM_VARIANTS) {
+		printf("Invalid variant selected (0-%zu)\n", NUM_VARIANTS-1);
+		return 1;
+	}
+
+	printf("Selected variant %d: %s\n", variant, radix_sorters[variant].name);
+
+	radix_sorter_fp radix_sorter = radix_sorters[variant].func;
 
 	void *input = NULL;
 	size_t len = 0;
@@ -200,7 +136,7 @@ int main(int argc, char *argv[]) {
 	printf("Input at %p, size=%zu\n", input, len);
 
 	const char** src = NULL;
-	size_t entries = generate_string_array(input, len, &src);
+	size_t entries = generate_string_ptrs(input, len, &src);
 
 	printf("%zu entries to sort.\n", entries);
 
@@ -211,25 +147,30 @@ int main(int argc, char *argv[]) {
 
 	const char** aux = malloc(entries * sizeof(const char*));
 
-	const char **res = radix_sort_CE0_CB_BM1(src, aux, entries, 0);
+	const char **res = radix_sorter(src, aux, entries, 0);
 
+#if STATS
 	int used = 0;
 	for (size_t i = 0 ; i < 256 ; ++i) {
 		if (bucket_use[i] > 0) {
-#if BUCKETUSE__
+#if BUCKETUSE
 			printf("bucket_use[%zu]=%zu\n", i, bucket_use[i]);
 #endif
 			++used;
 		}
 	}
 	printf("Buckets used %d, unused %d. %zu calls, %zu iterations (%zu wasted)\n", used, 256-used, calls, iters, wasted_iters);
+#endif
 
 #if VERIFY
 	for (size_t i = 0 ; i < entries ; ++i) {
 		if (i < 10)
 			printf("%s\n", res[i]);
+		else
+			break;
 	}
 
+	// TODO: Verify pointer output is permuation of pointer input.
 	int ok = 1;
 	for (size_t i=1 ; i < entries ; ++i) {
 		if (strcmp(res[i-1], res[i]) > 0) {
