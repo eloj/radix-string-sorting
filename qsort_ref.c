3b445c139921409bf5d9a1bfdb91bd6758c770cc
Eddy Jansson <eddy@klopper.net>
2021-12-13 00:46:09 +0100

    Make the reference qsort capable of sorting suffixes.
    This allows us to transparently use it in e.g. hybrid
    implementations.

--- qsort_ref.c
+++ qsort_ref.c
@@ -1,11 +1,12 @@
-static int cmpstrp(const void *p1, const void *p2)
+static int cmpstrp(const void *p1, const void *p2, void *arg)
 {
+	const int h = *(int*)(arg);
 	STAT_INC_ITERS;
-	return strcmp(*(char* const *)p1, *(char* const *)p2);
+	return strcmp(*(char* const *)p1 + h, *(char* const *)p2 + h);
 }
 
 static const char** qsort_ref(const char** RESTRICT S, const char** RESTRICT T, size_t n, int h) {
 	STAT_INC_CALLS;
-	qsort(S, n, sizeof(char*), cmpstrp);
+	qsort_r(S, n, sizeof(char*), cmpstrp, &h);
 	return S;
 }
